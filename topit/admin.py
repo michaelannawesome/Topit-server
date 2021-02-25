33571325812de2a0d2103a4b128d768ac10e4149
michaelannawesome <michaelannawesome@gmail.com>
2021-02-25 10:47:08 -0800

    data model mirgrated

--- topit/admin.py
+++ topit/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Data
 
 # Register your models here.
 
@@ -16,3 +16,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Data)
--- topit/models.py
+++ topit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
@@ -7,4 +8,17 @@ class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
 
     def __str__(self):
-        return self.email
+        return self.name
+
+
+class Data(models.Model):
+    title = models.CharField(max_length=100)
+    source = models.CharField(max_length=200)
+    props = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    video_url = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="data")
+
+    def __str__(self):
+        return self.name
